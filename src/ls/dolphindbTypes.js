b623b4f505821c6930e9ad2165cf12800d0f16da
linfeng zhang <Linfengzh01@gmail.com>
2024-08-26 18:01:24 +0800

    compile failed due to dependencies messy

--- src/ls/dolphindbTypes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//dolphindb自定义客户端接口
-// export interface ddbClient
-// {
-//     connect():Promise<void>;
-//     close() : Promise<void>;
-//     query(query: string): Promise<any>;
-Object.defineProperty(exports, "__esModule", { value: true });
--- test/runTest.ts
+++ test/runTest.ts
@@ -1,36 +1,32 @@
-import dolphindbDriver from "../src/ls/driver"
-// import {strict as assert} from 'assert'
-import { IConnection } from '@sqltools/types'
-import { ddbConfig } from '../src/ls/dolphindbTypes'
-import { WorkspaceFolder } from 'vscode-languageserver-types'
-// import { error } from 'console'
+import { IConnection } from '@sqltools/types';
+import { ddbConfig } from '../src/ls/dolphindbTypes';
+import dolphindbDriver from "../src/ls/driver";
+
+
+
+const credentials: IConnection<ddbConfig> = {
+    server: '',
+    port: 8840,
+    username: 'admin',
+    password: '123456',
+    name: '',
+    driver: '',
+    id: '',
+    isConnected: true,
+    isActive: true
+};
+
+
 
 
 async function runTests() {
-    const credentails:IConnection<ddbConfig> = 
-    {
-        server:'',
-        port:8840,
-        username:'admin',
-        password:'123456',
-        name:'',
-        driver:'',
-        id:'',
-        isConnected:true,
-        isActive:true
-    }
-
-    const getWorkspaceFolders = () =>
-        {
-            return WorkspaceFolder[""]
-        }
-
-    const driver = new dolphindbDriver(credentails,getWorkspaceFolders);
-
+    const driver = new dolphindbDriver(credentials, [""][""]);
+    
     console.log('Testing...');
+    driver.open();
 }
-runTests().catch(error=>
-        {
-            console.error("something happened");
-            process.exit(1);
-        })
+
+runTests().catch(error => {
+    console.error("Something happened:", error);
+    process.exit(1);
+});
